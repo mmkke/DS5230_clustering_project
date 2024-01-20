44f5cdc74fca89b6b1f9f5cd745cf325b2fdc5e9
Michael Massone <mikey@Michaels-MBP.lan>
2024-01-20 14:41:23 -0500

    Updates to csv .py files.

---
+++ design_to_csv.py
@@ -0,0 +1,10 @@
+def design_to_csv(design_df, file_path):
+    '''Saves design matrix dataframe as csv in current directory.'''
+
+
+    design_df.to_csv(file_path.split('.')[0] + '_design.csv', index=False)
+
+    print(f"Design dataframe saved at: {file_path.split('.')[0]}_design.csv")
+
+    return
+
