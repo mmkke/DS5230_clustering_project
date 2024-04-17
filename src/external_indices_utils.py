0e2ac2248030efa38bcdb3c98a36f00838de082a
nfarrell011 <132219657+nfarrell011@users.noreply.github.com>
2024-04-16 22:25:39 -0400

    3.9

--- src/external_indices_utils.py
+++ src/external_indices_utils.py
@@ -1,3 +1,24 @@
+'''
+    Final Project
+    Joseph Nelson Farrell & Michael Missone
+    DS 5230 Unsupervised Machine Learning
+    Northeastern University
+    Steven Morin, PhD
+
+    This file contains a libray of functions used to compute the external indices of a clustering algorithm
+    solutions
+
+    Functions: (in order)
+        1. drop_minus_one_labels
+        2. get_modes
+        3. get_mapping
+        4. get_cont_matrix
+        5. get_cont_matrices
+        6. find_best_cont_matrix
+        7. convert_string_to_array
+        8. get_true_label_purity
+'''
+
 ## Libraries
 
 import time
@@ -130,7 +151,8 @@ def get_cont_matrix(true_labels, cluster_labels):
 ##############################################################################################################
 def get_cont_matrices(true_labels, cluster_labels, print_results=False, time_crit=False, duration=300):
     '''
-    Description: This function determines the possible adjusted rand score and contingency matrices based on c;luster_label permutations. 
+    Description: This function determines the possible adjusted rand score and contingency matrices based on 
+        cluster_label permutations. 
 
     Parameters:
                 cluster_labels (np.ndarray): labels detrmined by clustering algo=dbscan 
@@ -224,6 +246,71 @@ def find_best_cont_matrix(external_indicies_df):
     best_indicies = external_indicies_df.iloc[max_trace_idx, :]
 
     return best_indicies
+
+##############################################################################################################
+##############################################################################################################
+def convert_string_to_array(string: str) -> np.ndarray:
+    """
+        This function will convert a string to numpy array. When saving a pd.Frame to .csv if one the cells
+        contains a np.ndarray the default behavior is to convert the array to a string. This will reverse that
+        actions.
+
+        Parameters:
+            string: (str) - the element of the pd.Frame that should be a numpy array
+
+        Returns: 
+            num_array: (np.ndarray) - the string converted to a numpy array.
+    """
+    string = string.strip('[]').replace('\n', '')
+    num_list = string.split()
+    num_array = np.array([int(num) for num in num_list])
+    return num_array
+
+##############################################################################################################
+##############################################################################################################
+def get_true_label_purity(remapped_cont_matrix: np.ndarray, true_labels: pd.Series) -> tuple[pd.DataFrame, float]:
+    """
+        This function will compute the true label purity of a clustering results
+        contingency matrix.
+
+        Pamameters:
+            remapped_cont_matrix: (np.ndarray) - results contingency matrix
+
+        Returns:
+            purity_df: (pd.DataFrame) - a dataframe containing the purities of the all the true
+                labels
+            overall_purity: (float) - the overall purity of the contingency matrix
+    """
+    purity_dict = {}
+    correct_sum = 0
+    total = 0
+    idx_list = []
+
+    for i in range(remapped_cont_matrix.shape[1]):
+        column = remapped_cont_matrix[:, i]
+        col_max = np.max(column)
+        idx_max = np.argmax(column)
+        row = remapped_cont_matrix[idx_max, :]
+        row_sum = np.sum(row)
+        purity = col_max / row_sum
+        correct_sum += col_max
+        idx_list.append(idx_max)
+        purity_dict[idx_max] = {'true_label': idx_max, 'purity': purity}
+
+    true_labels_list = np.arange(0, len(np.unique(true_labels)))
+    missing_true_labels = [x for x in true_labels_list if x not in idx_list]
+
+    for x in missing_true_labels:
+        purity_dict[x] = {'true_label': x, 'purity': 0}
+
+    purity_df = pd.DataFrame(list(purity_dict.values()))
+
+    purity_df = purity_df.sort_values(by = 'true_label')
+    purity_df.set_index('true_label', inplace = True)
+
+    overall_purity = correct_sum / np.sum(remapped_cont_matrix)
+
+    return purity_df, overall_purity
 
 if __name__ == '__main__':
     pass
